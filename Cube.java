7e0bf3644f21e12e26d70731c19dee4caff95c33
Adam Carling <adamjcarling>
2022-09-30 17:42:57 -0600

    changed the whole program a lot

--- Cube.java
+++ Cube.java
@@ -3,12 +3,11 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class Cube {  
+
   public static void main(final String[] args){
     //This cube is a three dimensional array. it is of data type char but that might change later.
     //Maybe I need to declare the cube outside of main so that it is global? 
-    char cube[][][] = new char[6][3][3];
-    //This is the key for which number to which color
-    /*
+    /*This is the key for which number to which color
       0 = White
       1 = Green
       2 = Yellow
@@ -16,13 +15,13 @@ public class Cube {
       4 = Red 
       5 = Orange
     */
+    char cube[][][] = new char[6][3][3];
     for (int i = 0; i < 6; i++){
       //width
       for (int x = 0; x < 3; x++){
         //height
         for (int y = 0; y < 3; y++){
-          int face = i;
-          switch (face) {
+          switch (i) {
             case 0:
               cube[i][x][y] = 'W';
               break;
@@ -45,65 +44,10 @@ public class Cube {
         }
       }
     }
- 
-    //Setting up the input from the user
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    char command = ' ';
-    
-
-    try {
-      String input;
-      
-      while( command != 'x' & command != 'X'){
-        
-        display(cube);
-        System.out.print("Enter Data: ");
-        input = reader.readLine();
-        //***another While loop here for if the string is full of more commands. right now I will only take the first command.
-
-
-        command = input.charAt(0);
-        switch(command){
-          case 'u':
-          case 'U':
-           cube = uMove(cube);
-           break;
-          case 'd':
-          case 'D':
-            cube = dMove(cube);
-            break;
-          case 'r':
-          case 'R':
-            cube = rMove(cube);
-            break;
-          case 'l':
-          case 'L':
-            cube = lMove(cube);
-            break;
-          case 'f':
-          case 'F':
-            cube = fMove(cube);
-            break;
-          case 'b':
-          case 'B':
-            cube = bMove(cube);
-            break;
-        }
-//System.out.println("\nYou entered: " + command);
-     }//end While loop
-     System.out.print("Program Terminated.\n");
-    }//end try
-    catch(IOException e) {
-      System.out.println(e);
-      System.out.println("You faggot! How hard is it to hit the right button?");
-    }
-    catch(StringIndexOutOfBoundsException e){
-      System.out.println("You Retard! put in an actual command.");
-      //***Put in some sort of loop back so the code continues on even if this error happens.
-    }
-    
-    
-  }
+    String command = "";
+    run(cube, command);
+    System.out.println("Program Terminated.\n");
+  }// End Main
 
 //Mehtod that displays the cube when called. takes a 3d array as a varriable.
   public static void display(char temp[][][]){
@@ -124,10 +68,6 @@ public class Cube {
 //I could have display display faces and simply call it multiple times. 
 //***Add in a way to track which moves are happening so that we can reverse it later
 
-
-//**(Priority #1)** //This guy just needs to be copied for each of the next five moves. 
-//The command work but they all only rotate along one axis. fix the rotation so that the cube moves.
-
 //U move
   public static char[][][] uMove(char temp[][][]){
     char rowTemp[] = new char[3];
@@ -140,24 +80,24 @@ public class Cube {
       temp[2][0][x] = temp[5][0][x];
       temp[5][0][x] = rowTemp[x];
     }//end forLoop
+    faceRotation(temp, 3);
     return temp;
   }//End 
-
 //D Move
   public static char[][][] dMove(char temp[][][]){
     char rowTemp[] = new char[3];
-    for(int i=2; i>-1; i--){
-      rowTemp[i] = temp[0][0][i];
+    for(int i=0; i<3; i++){
+      rowTemp[i] = temp[0][2][i];
     }//End forLoop
-    for(int x=2; x>-1; x--){
+    for(int x=0; x<3; x++){
       temp[0][2][x] = temp[5][2][x];
       temp[5][2][x] = temp[2][2][x];
       temp[2][2][x] = temp[4][2][x];
       temp[4][2][x] = rowTemp[x];
     }//End forLoop
+    faceRotation(temp, 1);
     return temp;
   }//End
-
 //R Move
   public static char[][][] rMove(char temp[][][]){
     char rowTemp[] = new char[3];
@@ -170,9 +110,9 @@ public class Cube {
       temp[2][x][2] = temp[3][x][2];
       temp[3][x][2] = rowTemp[x];
     }//End forLoop
+    faceRotation(temp, 4);
     return temp;
   }//End
-
 //L Move
   public static char[][][] lMove(char temp[][][]){
     char rowTemp[] = new char[3];
@@ -185,9 +125,9 @@ public class Cube {
       temp[2][x][0] = temp[1][x][0];
       temp[1][x][0] = rowTemp[x];
     }//End forLoop
+    faceRotation(temp, 5);
     return temp;
   }//End
-
 //F Move
   public static char[][][] fMove(char temp[][][]){
     char rowTemp[] = new char[3];
@@ -195,14 +135,14 @@ public class Cube {
       rowTemp[i] = temp[3][0][i];
     }//End forLoop
     for(int x=0; x<3; x++){
-      temp[3][0][x] = temp[5][0][x];
-      temp[5][0][x] = temp[1][0][x];
-      temp[1][0][x] = temp[4][0][x];
-      temp[4][0][x] = rowTemp[x];
+      temp[3][0][x] = temp[5][x][0];
+      temp[5][x][0] = temp[1][0][x];
+      temp[1][0][x] = temp[4][x][0];
+      temp[4][x][0] = rowTemp[x];
     }//End forLoop
+    faceRotation(temp, 0);
     return temp;
   }//End
-
 //B Move
   public static char[][][] bMove(char temp[][][]){
     char rowTemp[] = new char[3];
@@ -210,11 +150,130 @@ public class Cube {
       rowTemp[i] = temp[3][0][i];
     }//End forLoop
     for(int x=0; x<3; x++){
-      temp[3][0][x] = temp[4][0][x];
-      temp[4][0][x] = temp[1][0][x];
-      temp[1][0][x] = temp[5][0][x];
-      temp[5][0][x] = rowTemp[x];
+      temp[3][2][x] = temp[4][x][2];
+      temp[4][x][2] = temp[1][2][x];
+      temp[1][2][x] = temp[5][x][2];
+      temp[5][x][2] = rowTemp[x];
     }//End forLoop
+    faceRotation(temp, 2);
     return temp;
   }//End
-}
+/*
+ * Next step will be to add in all the prime moves and to fix reverseSolve so that it know how to recognize prime moves
+ * I am going to cheat on the primes and simply have them use the normal moves three times but only log one move for the commandList
+ * 
+ * second step will be to figure out how to identify paterns (possibly states achieved from different moves) and to shorten those paterns 
+ * (i.e. D,D,D is the same as D')
+ * probably use "indexOf()" with wildcards inside to find patterns of 3. Wild cards will not help in identifying the pattern.
+ * 
+ * lastly, I will add in an args check to feed moves to the while loop. 
+ */
+
+
+//Rotate faces function
+  public static char[][][] faceRotation(char tempCube[][][], int faceToRotate){
+    char temp = tempCube[faceToRotate][0][0];
+    tempCube[faceToRotate][0][0] = tempCube[faceToRotate][2][0];
+    tempCube[faceToRotate][2][0] = tempCube[faceToRotate][2][2];
+    tempCube[faceToRotate][2][2] = tempCube[faceToRotate][0][2];
+    tempCube[faceToRotate][2][2] = temp;
+    temp = tempCube[faceToRotate][0][1];
+    tempCube[faceToRotate][0][1] = tempCube[faceToRotate][1][0];
+    tempCube[faceToRotate][1][0] = tempCube[faceToRotate][2][1];
+    tempCube[faceToRotate][2][1] = tempCube[faceToRotate][1][2];
+    tempCube[faceToRotate][1][2] = temp;
+    return tempCube;
+  }
+//Reversing all moves
+  public static void reverseSolve(char temp[][][], String commands){
+    String tempSolve ="";
+    tempSolve = commands.substring(commands.length());
+//System.out.println(tempSolve + "1");
+    for(int i = commands.length(); i>0 ;i--){
+      tempSolve = tempSolve.concat(commands.substring(i-1, i));
+//System.out.println(tempSolve + "2");
+    }
+    tempSolve = tempSolve.concat("x");
+//System.out.println(tempSolve + "3");
+    run(temp, tempSolve);
+  }
+//run the main code
+  public static void run(char cube[][][], String commands){
+    String commandList = "";
+    String input = "";
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    
+    while( !input.matches("x") & !input.matches("X")){
+      display(cube);
+
+      try {
+        
+        if(commands.length() < 1){
+        System.out.print("Enter Data: ");
+        input = reader.readLine();
+        //display(cube);
+        }else{
+          input = commands.substring(0, 1);
+          commands = commands.substring(1);
+          System.out.println("____________________");
+        }
+
+        //Check for which command has been called 
+        switch(input){
+          case "u":
+          case "U":
+            commandList = commandList.concat(input);
+            cube = uMove(cube);
+            break;
+          case "d":
+          case "D":
+            commandList = commandList.concat(input);
+            cube = dMove(cube);
+            break;
+          case "r":
+          case "R":
+            commandList = commandList.concat(input);
+            cube = rMove(cube);
+            break;
+          case "l":
+          case "L":
+            commandList = commandList.concat(input);
+            cube = lMove(cube);
+            break;
+          case "f":
+          case "F":
+            commandList = commandList.concat(input);
+            cube = fMove(cube);
+            break;
+          case "b":
+          case "B":
+            commandList = commandList.concat(input);
+            cube = bMove(cube);
+            break;
+          case "x":
+          case "X":
+            break;
+          case "Solve":
+          case "solve":
+            System.out.println(commandList);
+            reverseSolve(cube, commandList);
+            break;
+          default :
+            System.out.println("That is not a real command! Try again");
+            break;
+        }
+    }//end try
+    catch(IOException e) {
+      System.out.println(e);
+      System.out.println("You faggot! How hard is it to hit the right button?");
+    }
+    catch(StringIndexOutOfBoundsException e){
+      System.out.println("You Retard! put in an actual command.");
+    }//end catch
+   }//end While loop
+   //display(cube);
+   System.out.println("While loop ended");
+   
+  }//End Run Function
+
+}//End Class
