5c8270a80c2f8c93b222cdea82c1fb9535a29654
Adam Carling <adamjcarling@gmail.com>
2022-10-03 23:50:16 -0600

    Finished

--- Cube.java
+++ Cube.java
@@ -4,16 +4,10 @@ import java.io.IOException;
 
 //View ReadMe for info on commands
 
-
 /*  Steps until project is finished.
- * 1) Next step will be to add in all the prime moves and to fix reverseSolve so that it know how to recognize prime moves
- * I am going to cheat on the primes and simply have them use the normal moves three times but only log one move for the commandList
- * 
  * 2) second step will be to figure out how to identify paterns (possibly states achieved from different moves) and to shorten those paterns 
  * (i.e. D,D,D is the same as D')
  * probably use "indexOf()" with wildcards inside to find patterns of 3. Wild cards will not help in identifying the pattern.
- * 
- * 3) lastly, I will add in an args check to feed moves to the while loop. 
  */
 public class Cube {  
   public static void main(final String[] args){
@@ -57,6 +51,13 @@ public class Cube {
       }
     }
     String command = "";
+    
+    if(args.length > 0){
+      for(int i =0; i < args.length; i++){
+        command = command.concat(args[i]);
+
+      }//End forLoop
+    }//End if
     run(cube, command);
     System.out.println("Program Terminated.\n");
   }// End Main
@@ -130,7 +131,7 @@ public static void display(char temp[][][]){
   public static char[][][] lMove(char temp[][][]){
     char rowTemp[] = new char[3];
     for(int i=0; i<3; i++){
-      rowTemp[i] = temp[0][i][2];
+      rowTemp[i] = temp[0][i][0];
     }//End forLoop
     for(int x=0; x<3; x++){
       temp[0][x][0] = temp[3][x][0];
@@ -152,6 +153,12 @@ public static void display(char temp[][][]){
       temp[5][x][0] = temp[1][0][x];
       temp[1][0][x] = temp[4][x][0];
       temp[4][x][0] = rowTemp[x];
+      
+      
+      //temp[3][0][x] = temp[5][x][0];
+      //temp[5][x][0] = temp[1][0][x];
+      //temp[1][0][x] = temp[4][x][0];
+      //temp[4][x][0] = rowTemp[x];
     }//End forLoop
     faceRotation(temp, 0);
     return temp;
@@ -163,10 +170,10 @@ public static void display(char temp[][][]){
       rowTemp[i] = temp[3][0][i];
     }//End forLoop
     for(int x=0; x<3; x++){
-      temp[3][2][x] = temp[4][x][2];
+      temp[3][0][x] = temp[4][x][2];
       temp[4][x][2] = temp[1][2][x];
-      temp[1][2][x] = temp[5][x][2];
-      temp[5][x][2] = rowTemp[x];
+      temp[1][2][x] = temp[5][x][0];
+      temp[5][x][0] = rowTemp[x];
     }//End forLoop
     faceRotation(temp, 2);
     return temp;
@@ -195,150 +202,158 @@ public static void display(char temp[][][]){
     try {
       tempSolve = commands.substring(commands.length());
       System.out.println("We are taking the last command given which is: " + tempSolve);
-
       for(int i = commands.length(); i>0;i--){
             tempChar = commands.substring(i-1, i);
-          
           if(tempChar.contains("'") == true){
             tempChar = commands.substring(i-2, i);
-          }
-          
-        //System.out.println("Move#" + i  + " We are adding a normal move");
+          }//End if
         tempChar = returnOposite(tempChar);
         tempSolve = tempSolve.concat(tempChar);
-        //System.out.println("Current list of commands: " + tempSolve );
-      }
+      }//End forLoop
       tempSolve = tempSolve.concat("x");
     } catch(Exception e){
       System.out.println(e);
       System.out.println("We are getting stuck in the reverseSolve");
     }
+    System.out.println("\n" + "The Stuff:" + tempSolve + ":");
     run(temp, tempSolve);
-  }
+  }//End reverseSolve
 
 //run the main code
   public static void run(char cube[][][], String commands){
     String commandList = "";
     String input = "";
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    
+
     while( !input.matches("x") & !input.matches("X")){
       display(cube);
-
       try { 
         if(commands.length() < 1){
         System.out.print("Enter Data: ");
         input = reader.readLine();
         }else{
-          if(commands.substring(0, 2).contains("'") == true){
-            input = commands.substring(0,2);
-            commands = commands.substring(2);
-          }else{
-            input = commands.substring(0, 1);
-            commands = commands.substring(1);
-          }
-          System.out.println("____________________");
+          try{
+            if(commands.substring(0, 2).contains("'") == true){
+              input = commands.substring(0,2);
+              commands = commands.substring(2);
+            }else{
+              input = commands.substring(0, 1);
+              commands = commands.substring(1);
+            }//end Else
+            if( commands == ""){
+              commands = "x";
+            }//End if
+          }//End try block
+          catch(IndexOutOfBoundsException e) {
+            input = commands.substring(0);
+            commands = "";
+          }//End Catch
+          
+          System.out.println("____________________\n");
         }//End if else
-
-      //Check for which command has been called 
-        switch(input){
-          case "u'":
-          case "U'":
-            commandList = commandList.concat(input);
-            cube = uMove(cube);
-            cube = uMove(cube);
-            cube = uMove(cube);
-            break;
-          case "d'":
-          case "D'":
-            commandList = commandList.concat(input);
-            cube = dMove(cube);
-            cube = dMove(cube);
-            cube = dMove(cube);
-            break;
-          case "r'":
-          case "R'":
-            commandList = commandList.concat(input);
-            cube = rMove(cube);
-            cube = rMove(cube);
-            cube = rMove(cube);
-            break;
-          case "l'":
-          case "L'":
-            commandList = commandList.concat(input);
-            cube = lMove(cube);
-            cube = lMove(cube);
-            cube = lMove(cube);
-            break;
-          case "f'":
-          case "F'":
-            commandList = commandList.concat(input);
-            cube = fMove(cube);
-            cube = fMove(cube);
-            cube = fMove(cube);
-            break;
-          case "b'":
-          case "B'":
-            commandList = commandList.concat(input);
-            cube = bMove(cube);
-            cube = bMove(cube);
-            cube = bMove(cube);
-            break;
-          case "u":
-          case "U":
-            commandList = commandList.concat(input);
-            cube = uMove(cube);
-            break;
-          case "d":
-          case "D":
-            commandList = commandList.concat(input);
-            cube = dMove(cube);
-            break;
-          case "r":
-          case "R":
-            commandList = commandList.concat(input);
-            cube = rMove(cube);
-            break;
-          case "l":
-          case "L":
-            commandList = commandList.concat(input);
-            cube = lMove(cube);
-            break;
-          case "f":
-          case "F":
-            commandList = commandList.concat(input);
-            cube = fMove(cube);
-            break;
-          case "b":
-          case "B":
-            commandList = commandList.concat(input);
-            cube = bMove(cube);
-            break;
-          case "x":
-          case "X":
-            break;
-          case "S":
-          case "s":
-            System.out.println(commandList);
-            reverseSolve(cube, commandList);
-            break;
-          default :
+      }//end try
+      catch(IOException e) {
+        System.out.println(e);
+        System.out.println("You faggot! How hard is it to hit the right button?");
+      }//End Catch
+      catch(StringIndexOutOfBoundsException e){
+        System.out.println(e);
+        System.out.println("This Error is being called from the \"Try\" block in the run function.");
+        input = "x";
+        commands = "x";
+      }//End catch
+      
+      if(input == " "){
+        input = "x";
+      }//End if
+    //Check for which command has been called
+      switch(input){
+        case "u'":
+        case "U'":
+          commandList = commandList.concat(input);
+          cube = uMove(cube);
+          cube = uMove(cube);
+          cube = uMove(cube);
+          break;
+        case "d'":
+        case "D'":
+          commandList = commandList.concat(input);
+          cube = dMove(cube);
+          cube = dMove(cube);
+          cube = dMove(cube);
+          break;
+        case "r'":
+        case "R'":
+          commandList = commandList.concat(input);
+          cube = rMove(cube);
+          cube = rMove(cube);
+          cube = rMove(cube);
+          break;
+        case "l'":
+        case "L'":
+          commandList = commandList.concat(input);
+          cube = lMove(cube);
+          cube = lMove(cube);
+          cube = lMove(cube);
+          break;
+        case "f'":
+        case "F'":
+          commandList = commandList.concat(input);
+          cube = fMove(cube);
+          cube = fMove(cube);
+          cube = fMove(cube);
+          break;
+        case "b'":
+        case "B'":
+          commandList = commandList.concat(input);
+          cube = bMove(cube);
+          cube = bMove(cube);
+          cube = bMove(cube);
+          break;
+        case "u":
+        case "U":
+          commandList = commandList.concat(input);
+          cube = uMove(cube);
+          break;
+        case "d":
+        case "D":
+          commandList = commandList.concat(input);
+          cube = dMove(cube);
+          break;
+        case "r":
+        case "R":
+          commandList = commandList.concat(input);
+          cube = rMove(cube);
+          break;
+        case "l":
+        case "L":
+          commandList = commandList.concat(input);
+          cube = lMove(cube);
+          break;
+        case "f":
+        case "F":
+          commandList = commandList.concat(input);
+          cube = fMove(cube);
+          break;
+        case "b":
+        case "B":
+          commandList = commandList.concat(input);
+          cube = bMove(cube);
+          break;
+        case "x":
+        case "X":
+          break;
+        case "S":
+        case "s":
+          System.out.println(commandList);
+          reverseSolve(cube, commandList);
+          break;
+        default :
           System.out.println(input);  
-          System.out.println("That is not a real command! Try again");
-            break;
-        }
-    }//end try
-    catch(IOException e) {
-      System.out.println(e);
-      System.out.println("You faggot! How hard is it to hit the right button?");
-    }
-    catch(StringIndexOutOfBoundsException e){
-      System.out.println(e);
-      System.out.println("This Error is being called from the \"Try\" block in the run function.");
-    }//end catch
-   }//end While loop
-   //display(cube);
-   System.out.println("While loop ended");
+          System.out.println("An inccorect command was used. Please use: \t(u, d, l, r, f, or b)\n\t\t(u', d', l', r', f', or b') \n Type \"S\" to solve and \"x\" to exit.");
+          break;
+      }//End Switch
+    }//end While loop   
   }//End Run Function
 
 //Retrun opposite of a given command
@@ -401,5 +416,4 @@ public static void display(char temp[][][]){
     }//End Switch
     return i;
   }//End returnOpposite Function
-
 }//End Class
